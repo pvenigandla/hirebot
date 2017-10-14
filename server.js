ef43d2ac03b721856bf2b1eca3cc2ae5cfbd8d3d
Greenlock <pokedoxysm@gmail.com>
2017-10-14 17:50:33 -0400

    Static content work

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ function requireController(path) {
 
 var server = ExpressWrapper.builder()
     .withContentPath(process.cwd())
-    .with404Controller(requireController('./lib/controllers/views/NotFoundViewController.js'))
-    .with500Controller(requireController('./lib/controllers/views/ServerErrorViewController.js'))
+    .with404('notfound.html')
+    .with500('internalerror.html')
     .build()
 
 server.run()
