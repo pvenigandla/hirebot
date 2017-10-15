e39b47d139e71b906df1a8349bc86dc70f6a4b7c
Greenlock <pokedoxysm@gmail.com>
2017-10-15 02:46:26 -0400

    More commit

--- server.js
+++ server.js
@@ -8,11 +8,14 @@ function requireController(path) {
 
 var server = ExpressWrapper.builder()
     .withContentPath(process.cwd())
+    .withDatabase()
     .with404('notfound.html')
     .with500('internalerror.html')
     .addStaticView('/adminlogin', 'html/adminlogin.html')
+    .addStaticView('/applicantlogin', 'html/applicantlogin.html')
     .addStaticView('/', 'html/index.html')
     .addView('/admin', requireController('./lib/controllers/views/AdminViewController.js'))
+    .addView('/careers', requireController('./lib/controllers/views/CareersViewController.js'))
     .build()
 
 server.run()
